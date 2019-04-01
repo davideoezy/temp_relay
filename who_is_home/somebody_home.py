210b701995508e36d1c7494b07e84cb111672284
David Erving <david.erving@hotmail.com>
2019-04-01 22:14:56 +1100

    modify somebody_home function

--- who_is_home/somebody_home.py
+++ who_is_home/somebody_home.py
@@ -2,34 +2,37 @@ import os
 import time
 
 
-def last_seen():
-
-    time_last_seen = [time.time()-7200] * 10
-    last_seen = [999] * 10
+def last_seen(time_last_seen):
 
     for ip in range(1,10):
         hostname = '192.168.0.'+str(ip+50)
         response = os.system("ping -c 1 " + hostname)
 
         if response == 0:
-            time_last_seen[ip] = time.time()
+            time_last_seen[ip-1] = time.time()
 
-        last_seen[ip] = (time.time() - time_last_seen[ip])/60
+        last_seen[ip-1] = (time.time() - time_last_seen[ip-1])/60
 
     return last_seen
 
-def anybody_home():
+def anybody_home(time_last_seen):
     somebody_home = False
 
-    check = last_seen()
+    check = last_seen(time_last_seen)
 
-    if any(t < 20 for t in check):
+    if any(t < 20 for t in time_last_seen):
         somebody_home = True
 
     return somebody_home
 
 if __name__ == "__main__":
-    somebody_home = anybody_home()
+    
+    time_last_seen_init = [time.time()-7200] * 10
+    last_seen = [999] * 10
+    
+    while True:
+        somebody_home = anybody_home(time_last_seen_init)
+        time.sleep(60)
 
     
 
