030289dd0d2ad9178ba28a44a3474992ea4bb924
David Erving <david.erving@hotmail.com>
2019-04-02 15:33:04 +1100

    calc time since seen

--- who_is_home/somebody_home_2.py
+++ who_is_home/somebody_home_2.py
@@ -3,15 +3,37 @@ import arpreq
 import time
 
 wifi_interface = "enp3s0"
-devices = 4
+devices = 1
 
 def whos_home(devices):
-    home_list = [False] * devices
+    
+    home_list = [time.time(),0] * devices
 
     for ip in range(0,devices):
         test = arpreq.arpreq('192.168.0.' + str(ip+51))
         if test is not None:
-            home_list[ip] = True
+            home_list[ip] = time.time(),1
 
-    return time.time(), home_list
+    return home_list
 
+def time_since_seen(prev_reading, current_reading, prev_log, devices)
+
+    for ip in range(0,devices):
+        if current_reading[ip][1] == 1:
+            last_seen[ip] = time.time()
+        elif current_reading[ip][1] == 0:
+            last_seen[ip] = prev_log[ip]
+    return last_seen                
+
+if __name__ == "__main__":
+    home_list_prev = [0] * devices
+    home_list_curr = whos_home(4)
+
+    prev_log = (time.time() - 10) * devices
+
+    curr_log = time_since_seen(home_list_prev, home_list_curr, prev_log, devices)
+
+
+
+
+    #prev_log = curr_log
