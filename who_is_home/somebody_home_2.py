51350b258c7cff300bb92b919e3ac360755c3d7f
David Erving <david.erving@hotmail.com>
2019-04-02 14:43:58 +1100

    change scanning tool to arp

---
+++ who_is_home/somebody_home_2.py
@@ -0,0 +1,17 @@
+import subprocess
+import arpreq
+import time
+
+wifi_interface = "enp3s0"
+devices = 4
+
+def whos_home(devices)
+    home_list = [False] * devices
+
+    for ip in range(0:devices):
+        test = arpreq.arpreq('192.168.0.' + str(ip+51))
+        if test != "":
+            home_list[ip] = True
+
+    return time.time(), home_list
+
