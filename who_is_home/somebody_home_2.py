626bc32855b68e5d012adbb207e4ab8092c900b6
David Erving <david.erving@hotmail.com>
2019-04-02 14:50:59 +1100

    fix

--- who_is_home/somebody_home_2.py
+++ who_is_home/somebody_home_2.py
@@ -9,8 +9,8 @@ def whos_home(devices):
     home_list = [False] * devices
 
     for ip in range(0,devices):
-        test = arpreq.arpreq('192.168.0.' + str(ip+51))
-        if test != "":
+        test = arpreq.arpreq("'192.168.0." + str(ip+51) + "'")
+        if test is None = False:
             home_list[ip] = True
 
     return time.time(), home_list
