93f04bb16eb569e77b93a0eda6167df770d53174
David Erving <david.erving@hotmail.com>
2019-05-04 14:17:40 +1000

    clean folders

--- relay/run_heater.py
+++ relay/run_heater.py
@@ -1,3 +1,5 @@
+from gpiozero import LED
+from time import sleep
 import mysql.connector as mariadb
 import time
 
@@ -5,7 +7,11 @@ db_host = '192.168.0.10'
 db_host_port = '3306'
 db_user = 'rpi'
 db_pass = 'warm_me'
-db = 'temp_logger'
+db = 'temp_control'
+
+
+relay = LED(17)
+
 
 def get_db_data(query, host, port, user, passwd, db):
     con = mariadb.connect(host=host, port=port, user=user,
@@ -13,21 +19,13 @@ def get_db_data(query, host, port, user, passwd, db):
     cur = con.cursor()
     cur.execute(query)
 
-    output = -999
+    output = 999
 
     for row in cur:
         output = row[0]
 
     return output
 
-def temp_trigger(temp, threshold):
-
-    temp_low = 0
-
-    if temp < threshold:
-        temp_low = 1
-
-    return temp_low
 
 def insert_results(query, db_host, db_host_port, db_user, db_pass, db):
 
@@ -35,7 +33,7 @@ def insert_results(query, db_host, db_host_port, db_user, db_pass, db):
                           user=db_user, password=db_pass, database=db)
     cur = con.cursor()
     try:
-        cur.execute(insert_stmt)
+        cur.execute(query)
         con.commit()
     except:
         con.rollback()
@@ -44,37 +42,31 @@ def insert_results(query, db_host, db_host_port, db_user, db_pass, db):
 
 
 if __name__ == "__main__":
-
     while True:
+
         query = """
         SELECT
-        avg(temp)
-        FROM temperature 
-        WHERE ts > DATE_SUB(now(), INTERVAL 90 second)
+        heater_on
+        FROM heater_rules
+        WHERE ts > DATE_SUB(now(), INTERVAL 60 second)
         ORDER BY ts ASC
         """
 
-        temp = get_db_data(query, db_host, db_host_port, db_user, db_pass, db)
+        operate = get_db_data(query, db_host, db_host_port, db_user, db_pass, db)
 
-        query_manual_temp = """
-        SELECT
-        temp_setting,
-        FROM heater_controls 
-        ORDER BY ts ASC
-        """
-
-        manual_temp = get_db_data(query_manual_temp, db_host, db_host_port, db_user, db_pass, db)
-
-        temp_low = temp_trigger(temp, manual_temp)
+        if operate == 1:
+            relay.on()
+        else:
+            relay.off()
 
         insert_stmt = """
-        INSERT INTO temp_rule
-        (temp_low, temp, threshold)
+        INSERT INTO heater_log
+        (heater_on)
         VALUES
-        ({},{},{})""".format(temp_low, temp, manual_temp)
+        ({})""".format(operate)
 
         insert_results(insert_stmt, db_host, db_host_port, db_user, db_pass, db)
-
+        
         time.sleep(30)
 
-## create table temp_rule (temp_low int, temp float, threshold float, ts timestamp);
+        
