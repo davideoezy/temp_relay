99b4f2dadd76c82cd83de0c0870772f12d94ec4b
David Erving <david.erving@hotmail.com>
2019-05-15 22:12:18 +1000

    fix

--- host_app/main.py
+++ host_app/main.py
@@ -6,96 +6,93 @@ from web_app_helper import ThermostatWeb
 import datetime
 import time
 
-def main():
-
-    db_helper = db_helper()
-    rules_aggregator = rules_aggregator()
-    rules = rules()
-    anybody_home = anybody_home()
+db_helper = db_helper()
+rules_aggregator = rules_aggregator()
+rules = rules()
+anybody_home = anybody_home()
 
 
-    ## Get data
+## Get data
 
-    web = ThermostatWeb(db_helper)
+web = ThermostatWeb(db_helper)
 
-    web.run()
+web.run()
 
-    while True:
+while True:
 
-        currentTemperature = db_helper.get_temp()
+    currentTemperature = db_helper.get_temp()
 
-        manual_controls = db_helper.get_control_settings()
+    manual_controls = db_helper.get_control_settings()
 
-        # On/off time in DB?
-        # Can then create function to update times based on calcs/manual update
+    # On/off time in DB?
+    # Can then create function to update times based on calcs/manual update
 
-        MorningOn = datetime.time(6, 00)
-        NightOff = datetime.time(22, 30)
+    MorningOn = datetime.time(6, 00)
+    NightOff = datetime.time(22, 30)
 
-        # Target Temperature
-        TargetTemperature = manual_controls[0]
+    # Target Temperature
+    TargetTemperature = manual_controls[0]
 
-        # power switch
-        power = manual_controls[1]
-        print(manual_controls)
-
-
-        ## Derive
-    
-        home_list_curr = anybody_home.whos_home()
-
-        last_seen_curr = anybody_home.last_time_seen(home_list_curr)
-
-        time_since_connected = anybody_home.time_since_seen(last_seen_curr)
-        print(time_since_connected)
+    # power switch
+    power = manual_controls[1]
+    print(manual_controls)
 
 
-        ## Rules
+    ## Derive
 
-        temp_low = rules.temp_trigger(currentTemperature, TargetTemperature)
+    home_list_curr = anybody_home.whos_home()
 
-        somebody_home = rules.anybody_home(time_since_connected)
+    last_seen_curr = anybody_home.last_time_seen(home_list_curr)
 
-        operating_hours = rules.hours_operation(MorningOn, NightOff)
-        print(operating_hours)
-
-
-        # run all rules
-
-        turn_heater_on = rules_aggregator.aggregate_rules(
-            power, somebody_home, operating_hours, temp_low)
-
-        print(turn_heater_on)
-        ## Reset variables
-
-        anybody_home.last_seen_prev = last_seen_curr[:]
+    time_since_connected = anybody_home.time_since_seen(last_seen_curr)
+    print(time_since_connected)
 
 
-        ## Insert logs
+    ## Rules
 
-        insert_rule_info = """
-                INSERT INTO rule_info
-                (temp_low, temp, threshold, 
-                anybody_home, dev_1, dev_2, dev_3, dev_4, 
-                operating_hours, MorningOn, NightOff)
-                VALUES
-                ({},{},{},{},{},{},{},{},{},'{}','{}')""".format(temp_low, currentTemperature, 
-                                                            TargetTemperature, somebody_home, 
-                                                            time_since_connected[0], time_since_connected[1], 
-                                                            time_since_connected[2], time_since_connected[3], 
-                                                            operating_hours, MorningOn, NightOff)
+    temp_low = rules.temp_trigger(currentTemperature, TargetTemperature)
 
-        db_helper.insert_db_data(statement=insert_rule_info)
+    somebody_home = rules.anybody_home(time_since_connected)
 
-        insert_heater_rules = """
-                INSERT INTO heater_rules
-                (power, temp_low, operating_hours, anybody_home, heater_on)
-                VALUES
-                ({},{},{},{},{})""".format(power, temp_low, operating_hours, somebody_home, turn_heater_on)
+    operating_hours = rules.hours_operation(MorningOn, NightOff)
+    print(operating_hours)
 
-        db_helper.insert_db_data(statement=insert_heater_rules)
 
-        print("loop complete")
-        time.sleep(30)
+    # run all rules
 
-main()
+    turn_heater_on = rules_aggregator.aggregate_rules(
+        power, somebody_home, operating_hours, temp_low)
+
+    print(turn_heater_on)
+    ## Reset variables
+
+    anybody_home.last_seen_prev = last_seen_curr[:]
+
+
+    ## Insert logs
+
+    insert_rule_info = """
+            INSERT INTO rule_info
+            (temp_low, temp, threshold, 
+            anybody_home, dev_1, dev_2, dev_3, dev_4, 
+            operating_hours, MorningOn, NightOff)
+            VALUES
+            ({},{},{},{},{},{},{},{},{},'{}','{}')""".format(temp_low, currentTemperature, 
+                                                        TargetTemperature, somebody_home, 
+                                                        time_since_connected[0], time_since_connected[1], 
+                                                        time_since_connected[2], time_since_connected[3], 
+                                                        operating_hours, MorningOn, NightOff)
+
+    db_helper.insert_db_data(statement=insert_rule_info)
+
+    insert_heater_rules = """
+            INSERT INTO heater_rules
+            (power, temp_low, operating_hours, anybody_home, heater_on)
+            VALUES
+            ({},{},{},{},{})""".format(power, temp_low, operating_hours, somebody_home, turn_heater_on)
+
+    db_helper.insert_db_data(statement=insert_heater_rules)
+
+    print("loop complete")
+    time.sleep(30)
+
