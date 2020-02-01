7b21ac0479bc47368fd1fe8712c7763449c0995e
david <david.erving@hotmail.com>
2020-02-01 22:41:57 +1100

    test mqtt

--- host_backend/main_backend.py
+++ host_backend/main_backend.py
@@ -12,15 +12,29 @@ anybody_home = anybody_home()
 ## Initialise variables
 
 server_address = "192.168.0.10"
-sub_topic = "sensors/inside/temperature"
-heater_rules = "home/heater/control/rules"
-heater_rule_detail = "home/heater/control/details"
+topic = "home/inside/sensor/lounge"
+#heater_rules = "home/heater/control/rules"
+#heater_rule_detail = "home/heater/control/details"
 
-client = mqtt.Client("docker_temp_rules")
 
 
 MorningOn = datetime.time(6, 00)
 NightOff = datetime.time(22, 30)
+
+def publish_message(key, value):
+
+	topic = "home/inside/heater/control"
+
+	server_address="192.168.0.10" 
+
+	client_label = "docker_heater_control"
+	client = mqtt.Client(client_label)
+	client.connect(server_address, keepalive=60)
+
+	dict_msg = {str(key):value}
+	msg = json.dumps(dict_msg)
+
+	client.publish(topic,msg)	
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -28,13 +42,15 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe(sub_topic)
+    client.subscribe(topic)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+    topic = msg.topic
+    print(topic)
     data = str(msg.payload.decode("utf-8"))
-    currentTemperature = data.split("air_temp=")[1]
-    print(currentTemperature)
+    jsonData=json.loads(data)
+    currentTemperature = jsonData["temperature"]
 
     try:
         manual_controls = db_helper.get_control_settings()
@@ -84,27 +100,27 @@ def on_message(client, userdata, msg):
     try:
         anybody_home.last_seen_prev = last_seen_curr[:]      
 
-        heater_rules_str = "rules, , power=%s,temp_low=%s,operating_hours=%s,somebody_home,=%s,turn_heater_on=%s".format(power, temp_low, operating_hours, somebody_home, turn_heater_on)
-        heater_rule_detail_str = "rule_detail, ,temp_low=%s,currentTemperature=%s,TargetTemperature=%s,somebody_home=%s,dev_1=%s,dev_2=%s,dev_3=%s,dev_4=%s,operating_hours=%s,MorningOn=%s,NightOff=%s",format(
-            temp_low, currentTemperature, TargetTemperature, somebody_home, time_since_connected[0], time_since_connected[1], 
-            time_since_connected[2], time_since_connected[3], operating_hours, MorningOn, NightOff)
+        #heater_rules_str = "rules, , power=%s,temp_low=%s,operating_hours=%s,somebody_home,=%s,turn_heater_on=%s".format(power, temp_low, operating_hours, somebody_home, turn_heater_on)
+        #heater_rule_detail_str = "rule_detail, ,temp_low=%s,currentTemperature=%s,TargetTemperature=%s,somebody_home=%s,dev_1=%s,dev_2=%s,dev_3=%s,dev_4=%s,operating_hours=%s,MorningOn=%s,NightOff=%s",format(
+        #    temp_low, currentTemperature, TargetTemperature, somebody_home, time_since_connected[0], time_since_connected[1], 
+        #    time_since_connected[2], time_since_connected[3], operating_hours, MorningOn, NightOff)
 
-        print(heater_rules_str)
-        print(heater_rule_detail_str)
+        #print(heater_rules_str)
+        #print(heater_rule_detail_str)
 
-        client.publish(heater_rules,str(heater_rules_str))
-        client.publish(heater_rule_detail, str(heater_rule_detail_str))
+        #client.publish(heater_rules,str(heater_rules_str))
+        #client.publish(heater_rule_detail, str(heater_rule_detail_str))
+        publish_message("turn_heater_on", turn_heater_on)
+
         
     except:
         print("error_3")
+
+client = mqtt.Client()
 
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect(server_address)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
 client.loop_start()
