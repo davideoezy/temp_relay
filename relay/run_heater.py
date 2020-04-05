fcfa0f07f66aa88b0d711b284327393c512de9b5
david <david.erving@hotmail.com>
2020-04-05 18:47:25 +1000

    add heat running duration

--- relay/run_heater.py
+++ relay/run_heater.py
@@ -2,6 +2,7 @@ from gpiozero import LED
 from mqtt_helper import mqtt_helper
 import paho.mqtt.client as mqtt
 import json
+from datetime import datetime
 
 relay = LED(17)
 location = "heater_relay"
@@ -13,7 +14,7 @@ mqtt_helper = mqtt_helper(location)
 topic_run_heater = "home/inside/control/heater"
 topic_heater_running = "home/inside/control/heater_running"
 
-heater_on = 0
+heater_on_curr = 0
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -27,19 +28,30 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    global heater_on
+    global heater_on_curr
 
     data = str(msg.payload.decode("utf-8"))
     jsonData=json.loads(data)    
 
-    heater_on = jsonData["heater_on"]
+    heater_on_new = jsonData["heater_on"]
+    time_running = 0
 
-    if heater_on == 1:
+    if heater_on_new == 1:
+        if heater_on_curr == 0:
+            time_start = datetime.now()
+
         relay.on()
-    else:
-        relay.off()
 
-    dict_msg = {"heater_running": heater_on}
+    else:
+        if heater_on_curr == 1:
+            time_end = datetime.now()
+            time_running = (time_end - time_start).total_seconds()
+  
+        relay.off()
+    
+    heater_on_curr = heater_on_new
+
+    dict_msg = {"heater_running": heater_on_curr, 'time_running': time_running}
     mqtt_helper.publish_generic_message(topic_heater_running, dict_msg)
 
     mqtt_helper.publish_status()
